72e7fc1ee5d6bd0d3a1866f8e5b8ab21c314e2fb
pharaoh <pharaohmanson@gmail.com>
2021-04-02 18:05:44 -0400

    First commit

---
+++ Hangman_project/main.py
@@ -0,0 +1,12 @@
+import random
+word_list = ['money', 'baboon', 'bacon']
+
+chosen_word = random.choice(word_list)
+
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
