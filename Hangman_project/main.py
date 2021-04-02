165995771928aa7bedfda6cb310b9619021cefe6
pharaoh <pharaohmanson@gmail.com>
2021-04-02 18:32:33 -0400

    added logic if letter the user guess is correct

--- Hangman_project/main.py
+++ Hangman_project/main.py
@@ -1,12 +1,17 @@
 import random
 word_list = ['money', 'baboon', 'bacon']
-
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+display = []  # list
+for letter in range(word_length):
+    display += "_"
 
+print(f"word is {chosen_word}")
+print(display)
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+        print(display)
