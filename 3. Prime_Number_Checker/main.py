2765768653e447758482b5285575b2b29859aceb
pharaoh <pharaohmanson@gmail.com>
2021-04-17 20:31:12 -0400

    casear cipher finished

--- 3. Prime_Number_Checker/main.py
+++ 3. Prime_Number_Checker/main.py
@@ -2,11 +2,9 @@ def check_num(num):
     counter = 0
     if num == 0:
         return print("Can not use 0")
-
     for i in range(1, num + 1):
         if num % i == 0:
             counter += 1
-
     if counter <= 2:
         print(f"{num} is Prime number")
     else:
---
+++ 4. Caesar_Cipher/main.py
@@ -0,0 +1,35 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+userDirectionInput = input(
+    "Type 'encode' to encrypt, or type 'decode' to decrypt:\n")
+userTextInput = input("Type your message:\n").lower()
+userShiftInput = int(input("Type the shift number: \n"))
+
+# Create a function called encrypt that takes the text and shifts inputs
+
+
+def encrypt(userTextInput1, userShiftInput1):
+    #: Inside the encrypt function shift each letter of the text forwards in the alphabet
+    cipher_text = ""
+    for letter in userTextInput:
+        position = alphabet.index(letter)
+        new_position = position + userShiftInput
+        cipher_text += alphabet[new_position]
+
+    print(f"The encoded text is {cipher_text}")
+
+
+def decrypt(cipher_text, userShiftInput2):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - userShiftInput
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
+
+
+if userDirectionInput == "encode":
+    encrypt(userTextInput1=userTextInput, userShiftInput1=userShiftInput)
+elif userDirectionInput == "decode":
+    decrypt(cipher_text1=userTextInput, userShiftInput2=userShiftInput)
