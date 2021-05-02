ff3a97293da6f62d5c62bbf16c2fcaca6d1dbe31
pharaoh <pharaohmanson@gmail.com>
2021-05-02 16:24:58 -0400

    finished quiz game

---
+++ 7. Quiz_Game/main.py
@@ -0,0 +1,17 @@
+from question_model import Questions
+from data import question_data
+from quiz_brain import QuizBrain
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Questions(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+quiz.finished()
--- 7. Quiz_Game/notes.py
+++ 7. Quiz_Game/notes.py
@@ -6,7 +6,14 @@ class User:
         self.username = username
         # Set this default value
         self.followers = 0
+        self.following = 0
+
+    # unlike functions, methods always has the self keyword so it knows what object called it
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "johne")
-print(user_1.username)
+user_2 = User("001", "Mikese")
+user_1.follow(user_2)
