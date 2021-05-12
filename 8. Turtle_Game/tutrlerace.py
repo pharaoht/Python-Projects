0513437ea58336c1698b9fa4dc41fa8d898193e6
pharaoh <pharaohmanson@gmail.com>
2021-05-12 16:34:49 -0400

    turtles are moving

--- 8. Turtle_Game/tutrlerace.py
+++ 8. Turtle_Game/tutrlerace.py
@@ -1,19 +1,29 @@
 # Objects that are performing different methods at any one time in programming is known as their state
 from turtle import Turtle, Screen
-
+import random
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(
     title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 ypositions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
 for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=ypositions[turtle_index])
+    all_turtles.append(new_turtle)
 
-    tim = Turtle(shape="turtle")
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(x=-230, y=ypositions[turtle_index])
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 
 screen.exitonclick()
