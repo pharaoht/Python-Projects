7027890ca55b4489f81c1e345803f091cf2592c0
pharaoh <pharaohmanson@gmail.com>
2021-06-07 20:53:20 -0400

    pong game

--- 10. Pong_game/main.py
+++ 10. Pong_game/main.py
@@ -6,11 +6,6 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
 
 
 def go_up():
