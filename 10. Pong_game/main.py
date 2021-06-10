daba96da203e5e2c82d6147987b0305393f2ab8a
pharaoh <pharaohmanson@gmail.com>
2021-06-10 17:14:30 -0400

    refactored paddle.py

--- 10. Pong_game/main.py
+++ 10. Pong_game/main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen, Paddle
+from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,22 +7,12 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+l_paddle = Paddle((-250, 0))
 
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
--- 10. Pong_game/paddle.py
+++ 10. Pong_game/paddle.py
@@ -10,3 +10,11 @@ class Paddle(Turtle):
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(350, 0)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
