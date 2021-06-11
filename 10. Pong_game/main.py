fe63fcd48143d02a0c0a1486ee31860ed5f2ef96
pharaoh <pharaohmanson@gmail.com>
2021-06-10 21:55:46 -0400

    added ball

--- 10. Pong_game/main.py
+++ 10. Pong_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
@@ -8,6 +9,7 @@ screen.title("Pong")
 screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 
 screen.listen()
