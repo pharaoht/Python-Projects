5809f9f5c8da50e04c8e032d11305f1e8bcd94f3
pharaoh <pharaohmanson@gmail.com>
2021-06-13 13:30:25 -0400

    point system added for left and right paddle

--- 10. Pong_game/main.py
+++ 10. Pong_game/main.py
@@ -32,11 +32,15 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # Detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
+    # Detect L paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
