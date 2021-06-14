6ff57d040d991ce513a426598bb7d80c8d9b1a22
pharaoh <pharaohmanson@gmail.com>
2021-06-14 19:56:47 -0400

    refactor

--- 9. Snake_game/scoreboard.py
+++ 9. Snake_game/scoreboard.py
@@ -6,6 +6,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -21,7 +22,6 @@ class Scoreboard(Turtle):
         self.clear()
         self.update_scoreboard()
 
-    def gameover(self):
-        self.goto(0, 0)
-        self.write(f"Game Over", align="center",
-                   font=("Arial", 24, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
