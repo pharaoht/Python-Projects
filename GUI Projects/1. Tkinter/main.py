2a9ef1a2ebd9023373277141c64ed940bbc20550
pharaoh <pharaohmanson@gmail.com>
2021-06-15 18:48:41 -0400

    working with tkinter widets

--- GUI Projects/1. Tkinter/main.py
+++ GUI Projects/1. Tkinter/main.py
@@ -1,12 +1,23 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
 
+
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+input = Entry()
+input.pack()
 
 window.mainloop()
--- GUI Projects/1. Tkinter/playground.py
+++ GUI Projects/1. Tkinter/playground.py
@@ -8,3 +8,24 @@ def add(*args):
 
 print(add(2, 3, 4, 5, 56, 6, 7, 6, 86, 57, 47, 4, 6, 435, 36, 376, 63, 4634, 6354, 63,
           63, 2, 4, 43, 45, 6, 65436, -43, -6543, -34, 54, -44, -65, -54, -342, -2434, -43353))
+
+
+def calculate(**kwargs):
+    print(kwargs["add"])
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+
+
+calculate(add="3", multiply="7")
+
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        # the benefit of get() is that if there isnt a key called model in the dictionary it wont throw an error, it will return null
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
