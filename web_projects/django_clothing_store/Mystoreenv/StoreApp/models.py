5ce7f36c9b2fbe5a35f108e1fafdc95428ddc168
pharaoh <pharaohmanson@gmail.com>
2021-06-19 22:52:21 -0400

    fields for user and address

---
+++ web_projects/django_clothing_store/Mystoreenv/StoreApp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Address(models.Model):
+    address_line_1 = models.CharField(max_length=100)
+    address_line_2 = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=5)
+    country = models.CharField(
+        default='United States', editable=False, max_length=13)
+
+
+class User(models.Model):
+    # ID is added by default in Django
+    # Add validator to not allow special characters
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    middle_int = models.CharField(max_length=1)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=30)
+    phone = models.CharField(max_length=10)
+    photo = models.ImageField(upload_to='uploads/')
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserManager(models.Manager):
+    pass
+
+
+class AddressManager(models.Model):
+    pass
